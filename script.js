a23e6a0131a7beda23061ee8f9766555d6ec32f7
Daniel Suarez <danfecisf@gmail.com>
2025-11-11 22:48:58 -0500

    solucion tarea 3

--- script.js
+++ script.js
@@ -1,32 +1,140 @@
 // Array con el abecedario y sus imágenes
 const abecedario = [
-    { letra: "A", imagen: "https://cdn-icons-png.flaticon.com/512/5040/5040698.png ", palabra: "Avión" },
-    { letra: "B", imagen: "https://cdn-icons-png.flaticon.com/512/5226/5226024.png ", palabra: "Ballena" },
-    { letra: "C", imagen: "https://cdn-icons-png.flaticon.com/512/4552/4552946.png ", palabra: "Casa" },
-    { letra: "D", imagen: "https://cdn-icons-png.flaticon.com/512/5226/5226046.png ", palabra: "Dedo" },
-    { letra: "E", imagen: "https://cdn-icons-png.flaticon.com/512/5310/5310626.png ", palabra: "Elefante" },
-    { letra: "F", imagen: "https://cdn-icons-png.flaticon.com/512/5226/5226066.png ", palabra: "Fuego" },
-    { letra: "G", imagen: "https://cdn-icons-png.flaticon.com/512/3479/3479417.png ", palabra: "Gato" },
-    { letra: "H", imagen: "https://cdn-icons-png.flaticon.com/512/4019/4019787.png  ", palabra: "huevo" },
-    { letra: "I", imagen: "https://cdn-icons-png.flaticon.com/512/5226/5226092.png  ", palabra: "Iglesia" },
-    { letra: "J", imagen: "https://cdn-icons-png.flaticon.com/512/4552/4552981.png ", palabra: "Jirafa" },
-    { letra: "K", imagen: "https://cdn-icons-png.flaticon.com/512/4552/4552989.png", palabra: "Karate" },
-    { letra: "L", imagen: "https://cdn-icons-png.flaticon.com/512/4552/4552996.png", palabra: "Lino" },
-    { letra: "M", imagen: "https://cdn-icons-png.flaticon.com/512/4552/4552998.png", palabra: "Mani" },
-    { letra: "N", imagen: "https://cdn-icons-png.flaticon.com/512/4553/4553006.png", palabra: "Niño" },
-    { letra: "Ñ", imagen: "https://cdn-icons-png.flaticon.com/512/5226/5226123.png", palabra: "Ñoño" },
-    { letra: "O", imagen: "https://cdn-icons-png.flaticon.com/512/3479/3479461.png", palabra: "Ocho" },
-    { letra: "P", imagen: "https://cdn-icons-png.flaticon.com/512/4553/4553011.png", palabra: "Pera" },
-    { letra: "Q", imagen: "https://cdn-icons-png.flaticon.com/512/4553/4553013.png", palabra: "Queso" },
-    { letra: "R", imagen: "https://cdn-icons-png.flaticon.com/512/4553/4553016.png", palabra: "Ratón" },
-    { letra: "S", imagen: "https://cdn-icons-png.flaticon.com/512/4553/4553018.png", palabra: "Sapo" },
-    { letra: "T", imagen: "https://cdn-icons-png.flaticon.com/512/4553/4553019.png", palabra: "Tetero" },
-    { letra: "U", imagen: "https://cdn-icons-png.flaticon.com/512/4553/4553022.png", palabra: "Uva" },
-    { letra: "V", imagen: "https://cdn-icons-png.flaticon.com/512/4553/4553024.png", palabra: "Venado" },
-    { letra: "W", imagen: "https://cdn-icons-png.flaticon.com/512/4553/4553026.png", palabra: "water" },
-    { letra: "X", imagen: "https://cdn-icons-png.flaticon.com/512/8532/8532367.png", palabra: "x" },
-    { letra: "Y", imagen: "https://cdn-icons-png.flaticon.com/512/4553/4553030.png", palabra: "Yuca" },
-    { letra: "z", imagen: "https://cdn-icons-png.flaticon.com/512/4553/4553032.png", palabra: "Zapato" },
+  {
+    letra: "A",
+    imagen: "https://cdn-icons-png.flaticon.com/512/5040/5040698.png ",
+    palabra: "Avión",
+  },
+  {
+    letra: "B",
+    imagen: "https://cdn-icons-png.flaticon.com/512/5226/5226024.png ",
+    palabra: "Ballena",
+  },
+  {
+    letra: "C",
+    imagen: "https://cdn-icons-png.flaticon.com/512/4552/4552946.png ",
+    palabra: "Casa",
+  },
+  {
+    letra: "D",
+    imagen: "https://cdn-icons-png.flaticon.com/512/5226/5226046.png ",
+    palabra: "Dedo",
+  },
+  {
+    letra: "E",
+    imagen: "https://cdn-icons-png.flaticon.com/512/5310/5310626.png ",
+    palabra: "Elefante",
+  },
+  {
+    letra: "F",
+    imagen: "https://cdn-icons-png.flaticon.com/512/5226/5226066.png ",
+    palabra: "Fuego",
+  },
+  {
+    letra: "G",
+    imagen: "https://cdn-icons-png.flaticon.com/512/3479/3479417.png ",
+    palabra: "Gato",
+  },
+  {
+    letra: "H",
+    imagen: "https://cdn-icons-png.flaticon.com/512/4019/4019787.png  ",
+    palabra: "huevo",
+  },
+  {
+    letra: "I",
+    imagen: "https://cdn-icons-png.flaticon.com/512/5226/5226092.png  ",
+    palabra: "Iglesia",
+  },
+  {
+    letra: "J",
+    imagen: "https://cdn-icons-png.flaticon.com/512/4552/4552981.png ",
+    palabra: "Jirafa",
+  },
+  {
+    letra: "K",
+    imagen: "https://cdn-icons-png.flaticon.com/512/4552/4552989.png",
+    palabra: "Karate",
+  },
+  {
+    letra: "L",
+    imagen: "https://cdn-icons-png.flaticon.com/512/4552/4552996.png",
+    palabra: "Lino",
+  },
+  {
+    letra: "M",
+    imagen: "https://cdn-icons-png.flaticon.com/512/4552/4552998.png",
+    palabra: "Mani",
+  },
+  {
+    letra: "N",
+    imagen: "https://cdn-icons-png.flaticon.com/512/4553/4553006.png",
+    palabra: "Niño",
+  },
+  {
+    letra: "Ñ",
+    imagen: "https://cdn-icons-png.flaticon.com/512/5226/5226123.png",
+    palabra: "Ñoño",
+  },
+  {
+    letra: "O",
+    imagen: "https://cdn-icons-png.flaticon.com/512/3479/3479461.png",
+    palabra: "Ocho",
+  },
+  {
+    letra: "P",
+    imagen: "https://cdn-icons-png.flaticon.com/512/4553/4553011.png",
+    palabra: "Pera",
+  },
+  {
+    letra: "Q",
+    imagen: "https://cdn-icons-png.flaticon.com/512/4553/4553013.png",
+    palabra: "Queso",
+  },
+  {
+    letra: "R",
+    imagen: "https://cdn-icons-png.flaticon.com/512/4553/4553016.png",
+    palabra: "Ratón",
+  },
+  {
+    letra: "S",
+    imagen: "https://cdn-icons-png.flaticon.com/512/4553/4553018.png",
+    palabra: "Sapo",
+  },
+  {
+    letra: "T",
+    imagen: "https://cdn-icons-png.flaticon.com/512/4553/4553019.png",
+    palabra: "Tetero",
+  },
+  {
+    letra: "U",
+    imagen: "https://cdn-icons-png.flaticon.com/512/4553/4553022.png",
+    palabra: "Uva",
+  },
+  {
+    letra: "V",
+    imagen: "https://cdn-icons-png.flaticon.com/512/4553/4553024.png",
+    palabra: "Venado",
+  },
+  {
+    letra: "W",
+    imagen: "https://cdn-icons-png.flaticon.com/512/4553/4553026.png",
+    palabra: "water",
+  },
+  {
+    letra: "X",
+    imagen: "https://cdn-icons-png.flaticon.com/512/8532/8532367.png",
+    palabra: "x",
+  },
+  {
+    letra: "Y",
+    imagen: "https://cdn-icons-png.flaticon.com/512/4553/4553030.png",
+    palabra: "Yuca",
+  },
+  {
+    letra: "z",
+    imagen: "https://cdn-icons-png.flaticon.com/512/4553/4553032.png",
+    palabra: "Zapato",
+  },
 ];
 
 function crearTarjetasAbecedario() {
@@ -64,16 +172,20 @@ function crearTarjetasAbecedario() {
       palabra.classList.add("texto-rojo");
     }
 
-    boton.addEventListener("click", () => {
-      alert(`Seleccionaste la letra ${item.letra} - ${item.palabra}`);
-    });
-
     contenedorBotonPalabra.appendChild(palabra);
     contenedorBotonPalabra.appendChild(boton);
 
     tarjeta.appendChild(img);
     tarjeta.appendChild(contenedorBotonPalabra);
     contenedor.appendChild(tarjeta);
+
+    boton.addEventListener("click", () => {
+      alert(`Seleccionaste la letra ${item.letra} - ${item.palabra}`);
+
+      const colores = ["green", "blue", "red"];
+      boton.style.backgroundColor =
+        colores[Math.floor(Math.random() * colores.length)];
+    });
   });
 }
 
